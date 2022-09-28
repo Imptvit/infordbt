5cc057c6d9551e7c6f012a31e417e402a7842030
vithalimpetus@gmail.com <vithalimpetus@gmail.com>
2022-09-28 08:18:57 +0000

    fixed SQL issues

--- models/infordbt/FINAL_ECONSOP03_VALIDITY1.sql
+++ models/infordbt/FINAL_ECONSOP03_VALIDITY1.sql
@@ -4,9 +4,9 @@
   )
 }}
 
-MERGE INTO {{ ref( 'FINAL_ECONSOP03_VALIDITY' ) }} AS T1 
-USING (SELECT  in1.out_Field_Name3 AS Field_Name, in1.out_Field_Value3 AS Field_Value 
-FROM {{ ref( 'RTR_TEST_UPDATE_1_0') }} AS in1 ) T2 ON T2.Field_Name = T1.out_Field_Name3 
- WHEN MATCHED THEN 
- UPDATE SET T1.out_Field_Name3 = T2.Field_Name,
- T1.OUT_FIELD_VALUE3 = T2.Field_Value 
+UPDATE {{ ref( 'FINAL_ECONSOP03_VALIDITY' ) }} AS T1 
+SET T1.Field_Name = T2.Field_Name,
+ T1.FIELD_VALUE = T2.Field_Value
+FROM (SELECT  in1.OUT_FIELD_NAME3 AS Field_Name, in1.OUT_FIELD_VALUE3  AS Field_Value 
+FROM {{ ref( 'RTR_TEST_UPDATE_1_0') }} AS in1 ) T2 
+WHERE T2.Field_Name = T1.Field_Name
--- models/infordbt/SIR_ECON_EXTRACT_LRF34002_META_PRIOR_0.sql
+++ models/infordbt/SIR_ECON_EXTRACT_LRF34002_META_PRIOR_0.sql
@@ -1,3 +1,3 @@
 SELECT 
  t.$1 AS "documentdatametainfo_payloadgroup_payloadinfo_payloadfilemetainfo_recordcoun"
-FROM @INPUT_STAGE/RDW_reclass__meta.csv t;
+FROM @INPUT_STAGE/RDW_reclass__meta.csv t
